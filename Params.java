2b831b305c8018dd9e66946527bcdaf63b33103c
Phoenix-77777 <3046252080@qq.com>
2024-11-24 19:00:18 +0800

    Add files via upload

---
+++ Params.java
@@ -0,0 +1,54 @@
+package com.example.entity;
+
+public class Params {
+        private String name;
+        private String number;
+        private Integer pageNum;
+        private Integer pageSize;
+        private Integer shoucang;
+
+
+
+        public String getName() {
+                return name;
+        }
+
+        public void setName(String name) {
+                this.name = name;
+        }
+
+        public String getNumber() {
+                return number;
+        }
+
+        public void setNumber(String number) {
+                this.number = number;
+        }
+
+        public Integer getPageNum() {
+                return pageNum;
+        }
+
+        public void setPageNum(Integer pageNum) {
+                this.pageNum = pageNum;
+        }
+
+        public Integer getPageSize() {
+                return pageSize;
+        }
+
+        public void setPageSize(Integer pageSize) {
+                this.pageSize = pageSize;
+        }
+
+        public Integer getShoucang() {
+                return shoucang;
+        }
+
+        public void setShoucang(Integer shoucang) {
+                this.shoucang = shoucang;
+        }
+
+
+
+}
